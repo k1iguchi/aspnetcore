848b9b6d601ce74225d59719bd54b68e041621d0
Iguchi Keiichi <keiichi@kei-1.jp>
2020-05-18 14:41:46 +0900

    EmailSender is a placeholder and does nothing. To send emails, you need to implement a class with <see cref="IEmailSender" /> interface.

--- src/Identity/UI/src/Areas/Identity/Services/EmailSender.cs
+++ src/Identity/UI/src/Areas/Identity/Services/EmailSender.cs
@@ -1,14 +1,19 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Identity.UI.Services
 {
+    /// <summary>
+    /// This EmailSender is a placeholder and does nothing. To send emails, you need to implement a class with <see cref="IEmailSender" /> interface.
+    /// </summary>
     internal class EmailSender : IEmailSender
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            Debug.WriteLine("This EmailSender is a placeholder and does nothing. To send emails, you need to implement a class with Microsoft.AspNetCore.Identity.UI.Services.IEmailSender interface.");
             return Task.CompletedTask;
         }
     }
